f9d5bda9e70ec12bb8bcf6190d06b4ab255fce00
ZynoxApp <zynoxapp@gmail.com>
2025-02-20 14:45:47 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,37 @@
-self.addEventListener("install", (event) => {
+self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open("pwa-cache").then((cache) => {
+    caches.open('pwa-cache').then((cache) => {
       return cache.addAll([
-        "/helllooo/index.html",
-        "/helllooo/",
-        "/helllooo/styles.css",  // CSS dosyan varsa ekle
-        "/helllooo/app.js",      // JS dosyan varsa ekle
-        "/helllooo/icon-192x192.png",
-        "/helllooo/icon-512x512.png"
+        '/helllooo/index.html',
+        '/helllooo/styles.css',
+        '/helllooo/app.js',
+        '/helllooo/manifest.json', // Manifest dosyasını da cache’e ekle
+        '/helllooo/icon-192x192.png',
+        '/helllooo/icon-512x512.png',
       ]);
     })
   );
 });
 
-self.addEventListener("fetch", (event) => {
+self.addEventListener('activate', (event) => {
+  // Kullanıcının eski önbelleği temizleniyor
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== 'pwa-cache') {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
       return response || fetch(event.request);
-    }).catch(() => {
-      return caches.match("/helllooo/index.html");
     })
   );
 });
